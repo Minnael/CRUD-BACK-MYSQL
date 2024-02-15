c2f7a6796d015b2f8b676cae72773905cdecb3c2
Minnael <minnaelcampelo@gmail.com>
2024-02-15 17:13:50 -0300

    preparando deploy

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express"
 import userRoutes from "./routes/users.js"
 import cors from "cors"
 
+const port = process.env.PORT || 8800;
+
 const app = express()
 
 app.use(express.json())
